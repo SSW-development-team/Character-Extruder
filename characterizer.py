35f211560993806129ba8e20ee72c705a9dfd604
yuto-moriizumi <moriizumi@design.inf.shizuoka.ac.jp>
2022-01-28 02:05:34 +0900

    全体の機能を関数化

--- characterizer.py
+++ characterizer.py
@@ -7,7 +7,7 @@ from IdeologyLeader import IdeologyLeader
 
 # キャラクターデータ抽出ツール
 # 1. 読み込むhoi4ファイル, キャラクターフォルダを指定する
-INPUT_FILE = R"C:\Users\YsikiShokurin\Programming\SSW_mod\history\countries\ANG - Angola.txt"
+INPUT_FILE = R"C:\Users\YsikiShokurin\Programming\SSW_mod\history\countries\ARO - Germany of Rosenberg.txt"
 CHARACTER_DIR = R"C:\Users\YsikiShokurin\Programming\SSW_mod\common\characters"
 # 2. このファイルを実行
 # 3. 読み込んだファイルの中身をoutput.txtで置き換える
@@ -22,7 +22,7 @@ def countryTagIdentifier(filename: str):
             return part
 
 
-def extruder(token: list[str]):
+def extruder(token: list[str], tag: str):
     """return the list of CountryLeader from tokens"""
     l_characters: dict[str, CountryLeader] = dict()
     # TARGET_SINGLE_TOKENS = ["picture", "expire", "ideology"]
@@ -92,80 +92,84 @@ def getTemplate(filename: str):
     return template
 
 
-os.chdir(os.getcwd())
+def character_extruder(filename: str):
+    """filenameを解析し、元ファイルを新方式に書き換え、キャラクターファイルを出力します"""
+    os.chdir(os.getcwd())
 
-mode = "replace"
-tag = countryTagIdentifier(INPUT_FILE)
-if tag == None:
-    print("カントリータグを判別できませんでした。実行ファイル下のフォルダに結果を出力します。")
-    mode = "non-replace"
-    tag = "TAG_REPLACE"
+    mode = "replace"
+    tag = countryTagIdentifier(filename)
+    if tag == None:
+        print("カントリータグを判別できませんでした。実行ファイル下のフォルダに結果を出力します。")
+        mode = "non-replace"
+        tag = "TAG_REPLACE"
 
-raw_txt = getTemplate(INPUT_FILE)
-output_path = f"output/ssw_{tag}.txt" if mode != "replace" else INPUT_FILE
-os.makedirs("output", exist_ok=True)
-out_file = open(output_path, "w", encoding="utf-8")
-chara_path = f"characters/ssw_{tag}.txt" if mode != "replace" else CHARACTER_DIR+f"\ssw_{tag}.txt"
-os.makedirs("characters", exist_ok=True)
-chara_file = open(chara_path, "a", encoding="utf-8")
+    raw_txt = getTemplate(filename)
+    output_path = f"output/ssw_{tag}.txt" if mode != "replace" else filename
+    os.makedirs("output", exist_ok=True)
+    out_file = open(output_path, "w", encoding="utf-8")
+    chara_path = f"characters/ssw_{tag}.txt" if mode != "replace" else CHARACTER_DIR+f"\ssw_{tag}.txt"
+    os.makedirs("characters", exist_ok=True)
+    chara_file = open(chara_path, "a", encoding="utf-8")
 
-ideology_template = getTemplate("templates/ideology.txt")
-character_template = getTemplate("templates/character.txt")
-root_template = getTemplate("templates/root.txt")
+    ideology_template = getTemplate("templates/ideology.txt")
+    character_template = getTemplate("templates/character.txt")
+    root_template = getTemplate("templates/root.txt")
 
-output = []
-nest_count = 0
-tokens_2_parse = ""
-characters: dict[str, CountryLeader] = dict()
+    output = []
+    nest_count = 0
+    tokens_2_parse = ""
+    characters: dict[str, CountryLeader] = dict()
+
+    for line in raw_txt.splitlines():
+        if "create_country_leader" in line:
+            nest_count += 1
+        elif 0 < nest_count and "{" in line:
+            nest_count += 1
+        if nest_count == 0:
+            output.append(line+"\n")
+        if 0 < nest_count and "}" in line:
+            nest_count -= 1
+        if nest_count > 0:
+            tokens_2_parse += line+"\n"
+        elif tokens_2_parse != "":
+            tokens_2_parse += "}"
+            token = ClauseWizard.cwparse(tokens_2_parse)
+            characters_local = extruder(token, tag)
+            # print(characters_local)
+            for chara_key in characters_local:  # グローバル辞書にすでにキャラが登録されている場合、まるごと上書きされるのを避ける
+                global_chara = characters.get(chara_key)
+                if global_chara == None:
+                    characters[chara_key] = characters_local[chara_key]
+                else:  # すでにキャラが登録されている場合は、イデオロギー情報のみを追加
+                    global_chara.ideologies.update(
+                        characters_local[chara_key].ideologies)
+            tokens_2_parse = ""
+            output.extend(
+                [f"recruit_character = {key}\n" for key in characters_local.keys()])
+
+    out_file.writelines(output)
+    out_file.close()
+
+    # 出力整形
+    character_txt = ""
+    for key in characters:
+        chara = characters[key]
+        ideology_txt = ""
+        for ideology_id in chara.ideologies:
+            ideology_data = chara.ideologies[ideology_id]
+            replacer = {"ideology": ideology_id,
+                        "expire": ideology_data.expire, "traits": "\n".join(ideology_data.traits)}
+            ideology_txt += Template(ideology_template).substitute(replacer)
+        replacer = {"id": chara.id,
+                    "picture": chara.picture, "ideology_leaders": ideology_txt}
+        character_txt += Template(character_template).substitute(replacer)
+    replacer = {"characters": character_txt}
+    root_txt = Template(root_template).substitute(replacer)
+
+    chara_file.write(root_txt)
+    chara_file.close()
+
+    print("実行が終了しました")
 
 
-for line in raw_txt.splitlines():
-    if "create_country_leader" in line:
-        nest_count += 1
-    elif 0 < nest_count and "{" in line:
-        nest_count += 1
-    if nest_count == 0:
-        output.append(line+"\n")
-    if 0 < nest_count and "}" in line:
-        nest_count -= 1
-    if nest_count > 0:
-        tokens_2_parse += line+"\n"
-    elif tokens_2_parse != "":
-        tokens_2_parse += "}"
-        token = ClauseWizard.cwparse(tokens_2_parse)
-        characters_local = extruder(token)
-        # print(characters_local)
-        for chara_key in characters_local:  # グローバル辞書にすでにキャラが登録されている場合、まるごと上書きされるのを避ける
-            global_chara = characters.get(chara_key)
-            if global_chara == None:
-                characters[chara_key] = characters_local[chara_key]
-            else:  # すでにキャラが登録されている場合は、イデオロギー情報のみを追加
-                global_chara.ideologies.update(
-                    characters_local[chara_key].ideologies)
-        tokens_2_parse = ""
-        output.extend(
-            [f"recruit_character = {key}\n" for key in characters_local.keys()])
-
-out_file.writelines(output)
-out_file.close()
-
-# 出力整形
-character_txt = ""
-for key in characters:
-    chara = characters[key]
-    ideology_txt = ""
-    for ideology_id in chara.ideologies:
-        ideology_data = chara.ideologies[ideology_id]
-        replacer = {"ideology": ideology_id,
-                    "expire": ideology_data.expire, "traits": "\n".join(ideology_data.traits)}
-        ideology_txt += Template(ideology_template).substitute(replacer)
-    replacer = {"id": chara.id,
-                "picture": chara.picture, "ideology_leaders": ideology_txt}
-    character_txt += Template(character_template).substitute(replacer)
-replacer = {"characters": character_txt}
-root_txt = Template(root_template).substitute(replacer)
-
-chara_file.write(root_txt)
-chara_file.close()
-
-print("実行が終了しました")
+character_extruder(INPUT_FILE)
